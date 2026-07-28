d65c3a7a9d53d0f18ccdf1df1ef955e919cfa1ff
Asfak Ansary <asfakansary369@gmail.com>
2026-07-28 21:36:32 +0000

    conversion of datatype

---
+++ learning_javascript/day2.js
@@ -0,0 +1,8 @@
+"use strict";//treat all coad as newer version
+console.log(3+3+12*2/4);
+console.log("ashu")
+
+
+
+
+console.log(typeof null)
---
+++ learning_javascript/day6.js
@@ -0,0 +1,8 @@
+let score ="99ab"
+
+console.log(typeof score)
+
+let A= Number(score)
+console.log(typeof A)
+console.log(A)
+
