685ab862b5b907ab95377253d08995d683b8e09e
Asfak Ansary <asfakansary369@gmail.com>
2026-07-30 17:59:21 +0000

    Datatype summery

---
+++ learning_javascript/Day7.js
@@ -0,0 +1,89 @@
+// let value =77
+// let alue=-value
+//console.log(alue);
+
+// console.log(3+4);
+// console.log(3-4);
+// console.log(3*4);
+// console.log(3/4);
+// console.log(3%4);
+
+// let A="Jade"
+// let B="Ashu"
+// let C=A+B
+//console.log(C);
+// console.log(+true);//1
+
+
+// console.log(-true);//-1
+// console.log(false);//false
+// console.log(+false);//0
+// console.log(-false);//-0
+// console.log(1+"");//1
+// console.log(1+true);//2
+
+
+
+
+// let A,B,C
+// A=B=C=369
+// console.log(A);
+// console.log(B);
+// console.log(C);
+
+
+
+// let gamecounter=369
+// gamecounter++;
+// console.log(gamecounter);
+
+
+
+// console.log(6+3>9);
+// console.log(7>6);
+// console.log(6+3>2);
+
+
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+
+// const id= symbol('123')
+// const anotherId= symbol('123')
+
+// console.log(id==anotherId);
+
+
+
+
+
+// const heroes=["Ironman","Spderman","Superman"];
+
+// let myobj={
+//     name:"ashu",
+//     age: 23,
+//     city: Kolkata,
+// 
+console.log(2+3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
